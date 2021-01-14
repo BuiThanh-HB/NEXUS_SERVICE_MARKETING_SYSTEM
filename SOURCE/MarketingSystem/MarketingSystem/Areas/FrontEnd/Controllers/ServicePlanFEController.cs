35bbe3fc1de7f0f7f79b7dd960a21e0a91859cc9
An <nguyenminhan696@gmail.com>
2021-01-14 13:36:27 +0700

    update web

--- SOURCE/MarketingSystem/MarketingSystem/Areas/FrontEnd/Controllers/ServicePlanFEController.cs
+++ SOURCE/MarketingSystem/MarketingSystem/Areas/FrontEnd/Controllers/ServicePlanFEController.cs
@@ -17,6 +17,18 @@ namespace MarketingSystem.Areas.FrontEnd.Controllers
             return View();
         }
 
+        public PartialViewResult SearchService(int Page = 1, string Name = "")
+        {
+            try
+            {
+                var data = servicePlanBusiness.GetListCategory();
+                return PartialView("_Service",data);
+            }
+            catch
+            {
+                return PartialView("_Service"/*new List<ListPostCategory>().ToPagedList(1, 1)*/ );
+            }
+        }
         //Đăng ký dịch vụ
         [HttpPost]
         public JsonResult CreateOrder(OrderInputModel input)
