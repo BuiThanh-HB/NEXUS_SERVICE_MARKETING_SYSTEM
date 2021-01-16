726a7270e4d0cd7f2946f1fd453528c33c9797cc
An <nguyenminhan696@gmail.com>
2021-01-16 11:32:06 +0700

    update news

--- SOURCE/MarketingSystem/MarketingSystem/Areas/FrontEnd/Controllers/NewsController.cs
+++ SOURCE/MarketingSystem/MarketingSystem/Areas/FrontEnd/Controllers/NewsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Model;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,18 @@ namespace MarketingSystem.Areas.FrontEnd.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult SearchNewsWeb(int Page = 1, string Name = "")
+        {
+            try
+            {
+                //var data = servicePlanBusiness.SearchFontEnd(Page, Name, 1, CateID);
+                return PartialView("_ListNewsWeb", new List<ListNewsOutputModel>().ToPagedList(1, 1));
+            }
+            catch
+            {
+                return PartialView("_ListNewsWeb", new List<ListNewsOutputModel>().ToPagedList(1, 1));
+            }
         }
     }
 }
