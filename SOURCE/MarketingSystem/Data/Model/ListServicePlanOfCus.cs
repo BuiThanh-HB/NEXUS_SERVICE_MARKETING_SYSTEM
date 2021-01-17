5efc39ee5a3fc3da30cb6ec3a6f9cf23f414316c
BuiThanh-HB <thanhstone98@gmail.com>
2021-01-17 17:23:26 +0700

    Update web admin

---
+++ SOURCE/MarketingSystem/Data/Model/ListServicePlanOfCus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Data.Model
+{
+    public class ListServicePlanOfCus
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string ServiceName { get; set; }
+        public string CusName { get; set; }
+        public int Status { get; set; }
+        public DateTime ActiveDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string LocaRequest { get; set; }
+    }
+}
